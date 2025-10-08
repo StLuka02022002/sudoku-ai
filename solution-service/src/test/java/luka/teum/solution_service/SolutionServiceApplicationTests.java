9d01055e9f027b14b23d912bff68e3124471f988
TEUM <sergey.lukyanenko02022002@mail.ru>
2025-10-08 13:25:47 +0300

    add solution-service

---
+++ solution-service/src/test/java/luka/teum/solution_service/SolutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package luka.teum.solution_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolutionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
